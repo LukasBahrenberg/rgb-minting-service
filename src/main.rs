04c9d2e3fa509c2e77088c5b79fed97c7c14f6f6
lukasbahrenberg <lukas.bahrenberg@gmail.com>
2023-06-12 09:57:29 +0200

    some refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,31 @@
 use std::{io::{self, Write}};
 use std::collections::hash_map::HashMap;
-use rgb_lib::{self, wallet::{self, Wallet, WalletData, DatabaseType, Recipient}, BitcoinNetwork};
+use rgb_lib::{self, wallet::{Wallet, WalletData, Online, DatabaseType, Recipient}, BitcoinNetwork};
 
 pub fn main() {
 
 	let electrum_url: &str = "tcp://localhost:50001";
-	let rgb_data_dir: String = "./rgb_data".to_string();
+	let rgb_data_dir: &str = "./rgb_data";
+
 	let mut consignment_endpoints = Vec::new();
 	consignment_endpoints.push("rgbhttpjsonrpc:http://localhost:3000/json-rpc");
 	const NETWORK: BitcoinNetwork = BitcoinNetwork::Regtest;
 
-    // prep the rgb wallet address for funding from miner
-    let (mut wallet, online_wallet) = prep_wallet(NETWORK, rgb_data_dir, electrum_url);
+    // prep the rgb wallet: create wallet, get funding address, connect to electrum server / create 'online_wallet'
+    let (wallet, online_wallet) = prep_wallet(NETWORK, rgb_data_dir, electrum_url).unwrap();
 	
     // start the cli minting service
-    start_cli(wallet, online_wallet, electrum_url, consignment_endpoints);
-
+    start_cli(wallet, online_wallet, consignment_endpoints);
 }
 
-fn prep_wallet(network: BitcoinNetwork, rgb_data_dir: String, electrum_url: &str) 
--> (Wallet, rgb_lib::wallet::Online)
+fn prep_wallet(network: BitcoinNetwork, rgb_data_dir: &str, electrum_url: &str) 
+-> Result<(Wallet, Online), Box<dyn std::error::Error>>
 {
+
+	// create wallet
     let keys = rgb_lib::generate_keys(network);
     let wallet_data = WalletData {
-        data_dir: rgb_data_dir,
+        data_dir: rgb_data_dir.to_string(),
         bitcoin_network: BitcoinNetwork::Regtest,
         database_type: DatabaseType::Sqlite,
         pubkey: keys.xpub,
@@ -33,16 +35,16 @@ fn prep_wallet(network: BitcoinNetwork, rgb_data_dir: String, electrum_url: &str
 	
 	// get funding address
 	let funding_address = wallet.get_address();
+	println!("This is an NFT minting service. You can mint RGB121 collectibles here");
     println!("This is the address to be funded from the miner: \n{}", funding_address);
 	
-	// connect to electrum server
+	// connect to electrum server / create 'online_wallet'
 	let online_wallet = wallet.go_online(false, electrum_url.to_string()).unwrap();
 					
-	(wallet, online_wallet)
+	Ok((wallet, online_wallet))
 }
 
-fn start_cli(mut wallet: Wallet, online_wallet: rgb_lib::wallet::Online, electrum_url: &str, consignment_endpoints: Vec<&str>) {
-	println!("This is the NFT minting service. You can mint RGB121 collectibles here");
+fn start_cli(mut wallet: Wallet, online_wallet: rgb_lib::wallet::Online, consignment_endpoints: Vec<&str>) {
     println!("First fund the wallet from the miner to the address above.");
 	loop {
 		print!("> ");
@@ -51,15 +53,15 @@ fn start_cli(mut wallet: Wallet, online_wallet: rgb_lib::wallet::Online, electru
 		if let Err(e) = io::stdin().read_line(&mut line) {
 			break println!("ERROR: {}", e);
 		}
-
 		if line.len() == 0 {
 			// We hit EOF / Ctrl-D
 			break;
 		}
-
 		let mut words = line.split_whitespace();
 		if let Some(word) = words.next() {
 			match word {
+				
+				// This command requires a funded wallet to work, then first creates utxos and then creates . Usage: `getblindedutxo`
 				"getblindedutxo" => {
 					
 					// create utxos
@@ -72,8 +74,10 @@ fn start_cli(mut wallet: Wallet, online_wallet: rgb_lib::wallet::Online, electru
 					println!("This is the blinded utxo: \n{:?}", blinded_utxo);
 					println!("Now enter in the other shell: `mint <nft-name> <metadata-path> <blinded-utxo>`");
 				},
-				"mint" => {
 
+				// This is the mint functionality the service offers. Usage: `mint <nft-name> <metadata-path> <blinded-utxo>`
+				"mint" => {
+					
 					// parse inputs
 					let nft_name = words.next();
 					let metadata_path = words.next();
@@ -88,13 +92,13 @@ fn start_cli(mut wallet: Wallet, online_wallet: rgb_lib::wallet::Online, electru
 					let nft_name = nft_name.unwrap().to_string();
 					let metadata_path = metadata_path.unwrap().to_string();
 					let blinded_utxo = blinded_utxo.unwrap().to_string();
-					println!("This is the passed in nft_name: {:?}", nft_name);
-					println!("This is the passed in metadata_path: {:?}", metadata_path);
-                    println!("This is the passed in blinded_utxo: {:?}", blinded_utxo);
+					// println!("This is the passed in nft_name: {:?}", nft_name);
+					// println!("This is the passed in metadata_path: {:?}", metadata_path);
+                    // println!("This is the passed in blinded_utxo: {:?}", blinded_utxo);
 
 					// create utxos
-					let utxos = wallet.create_utxos(online_wallet.clone(), true, Some(5), None, 1.5);
-					println!("This is the result of create_utxos: {:?}", utxos);
+					let _utxos = wallet.create_utxos(online_wallet.clone(), true, Some(5), None, 1.5);
+					// println!("This is the result of create_utxos: {:?}", _utxos);
 					
 					// issue asset
 					let rgb121_asset = wallet
@@ -108,7 +112,7 @@ fn start_cli(mut wallet: Wallet, online_wallet: rgb_lib::wallet::Online, electru
 							Some(metadata_path)
 						);
 					let asset_id = rgb121_asset.unwrap().asset_id;
-					println!("This is the resulting asset_id: {:?}", asset_id);
+					println!("This is the resulting asset_id: {:?}\n", asset_id);
 
 					// prep recipient
 					let recipient = Recipient {
@@ -118,16 +122,18 @@ fn start_cli(mut wallet: Wallet, online_wallet: rgb_lib::wallet::Online, electru
 					};
 					let mut recipient_map: HashMap<String, Vec<Recipient>> = HashMap::new();
 					recipient_map.insert(asset_id.clone(), vec![recipient]);
-					println!("This is the recipient_map: {:?}", recipient_map);
+					println!("This is the recipient_map: \n{:?}\n", recipient_map);
 					
 					// send asset
 					wallet.send(online_wallet.clone(), recipient_map, false, 1.5).unwrap();
-					println!("Assets should have been sent. This is the resulting list of transfers before mining.");
-					println!("{:?}", wallet.list_transfers(asset_id));
+					let transfer_list = wallet.list_transfers(asset_id).unwrap();
+					println!("Assets should have been sent. This is the resulting list of transfers before mining: \n{:?}\n", transfer_list);
 				},
+
+				// This command lists the transfers for a given asset id. Usage: `listtransfers <asset_id>`
 				"listtransfers" => {
 					
-					// list transfers after refresh
+					// parse inputs
 					let asset_id = words.next();
 					if asset_id.is_none()
 					{
@@ -136,14 +142,17 @@ fn start_cli(mut wallet: Wallet, online_wallet: rgb_lib::wallet::Online, electru
 					}
 					let asset_id = asset_id.unwrap().to_string();
 
+					// first refresh the wallet
 					let _ = wallet.refresh(online_wallet.clone(), None, Vec::new());
-					println!("This is the resulting list of transfers: ");
-					println!("{:?}", wallet.list_transfers(asset_id));
+					
+					// list transfers
+					let listed_transfers = wallet.list_transfers(asset_id).unwrap();
+					println!("These are the resulting transfers:\n{:?}", listed_transfers);
 				},
 				"quit" | "exit" => break,
 				_ => {
                     println!("Unknown command. Try again.");
-                    println!("Enter: `getblindedutxo` \n or `mint <nft-name> <metadata-path> <blinded-utxo>`");
+                    println!("Enter: `getblindedutxo` \n or `mint <nft-name> <metadata-path> <blinded-utxo>` \n or `listtransfers <asset_id>`");
                 },
 			}
 		}
